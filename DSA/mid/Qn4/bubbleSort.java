518c1f74957379928760170df347d6f0c080a7ff
BruhTes <162058640+BruhTes@users.noreply.github.com>
2024-05-13 16:27:08 +0300

    bubbleSort.java

--- DSA/mid/Qn4/bubbleSort.java
+++ DSA/mid/Qn4/bubbleSort.java
@@ -1,21 +1,21 @@
 public class bubbleSort {
     public static void main(String[] args){
-        int[] alphabets = new int[26];
-        alphabets[0] = a;
+        char[] alphabets = new char[26];
+        alphabets[0] = 'a';
     
         for (int i = 1; i < alphabets.length; i++) {
-            alphabets[i] = alphabets[i-1]++;
+            alphabets[i] = (char) alphabets[i-1]++;
         }
 
         bubbleSort(alphabets);
     }
 
-    static void bubbleSort(int[] array) {
+    static void bubbleSort(char[] array) {
         for (int i = array.length - 1; i>=0; i--) {
             for (int j = 0; j < i ; j++) {
-                array[j] = array[j] ^ array[j + 1];
-                array[j + 1] = array[j] ^ array[j + 1];
-                array[j] = array[j] ^ array[j + 1];
+                array[j] = array[j] + array[j + 1];
+                array[j + 1] = array[j] - array[j + 1];
+                array[j] = array[j] - array[j + 1];
             }
         }
     }
